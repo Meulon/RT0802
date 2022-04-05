6746e34149eecddf61156a74eed2796a6282c0df
meulon <marlon@ik.me>
2022-04-05 20:43:59 +0200

    fix

--- signCert.py
+++ signCert.py
@@ -6,7 +6,8 @@ from cryptography import x509
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.backends import default_backend
-import datetime 
+import datetime
+from datetime import timedelta 
 
 def sign_certificate_request(csr_cert, ca_cert, private_ca_key):
     cert = x509.CertificateBuilder().subject_name(
