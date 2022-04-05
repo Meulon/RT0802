ed17fd9d825023008e2654e687212cd2fb66c1b3
meulon <marlon@ik.me>
2022-04-05 10:42:19 +0200

    fix

--- CA.py
+++ CA.py
@@ -1,11 +1,12 @@
 #!/usr/local/bin/python3
 
+from tkinter import W
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
 from cryptography import x509
 from cryptography.x509.oid import NameOID
 from cryptography.hazmat.primitives import hashes
-from datetime import datetime 
+from datetime import datetime, timedelta,  
 
 # Generate our key
 key = rsa.generate_private_key(
