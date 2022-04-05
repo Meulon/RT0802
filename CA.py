604d44cf61ca1ab05ba34ed1e52605da86306e15
meulon <marlon@ik.me>
2022-04-05 10:42:39 +0200

    fix

--- CA.py
+++ CA.py
@@ -6,7 +6,7 @@ from cryptography.hazmat.primitives import serialization
 from cryptography import x509
 from cryptography.x509.oid import NameOID
 from cryptography.hazmat.primitives import hashes
-from datetime import datetime, timedelta,  
+from datetime import datetime, timedelta 
 
 # Generate our key
 key = rsa.generate_private_key(
