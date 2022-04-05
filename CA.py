c66bffac1d799acde8fcf13a9782bd9a26d3b5a2
meulon <marlon@ik.me>
2022-04-05 10:36:44 +0200

    fix

--- CA.py
+++ CA.py
@@ -11,9 +11,46 @@ key = rsa.generate_private_key(
 
 # Write our key to disk for safe keeping
 
-with open("/home/toto/ssh/key.pem", "wb") as f:
+with open("/home/toto/crypto/key.pem", "wb") as f:
     f.write(key.private_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PrivateFormat.TraditionalOpenSSL,
         encryption_algorithm=serialization.BestAvailableEncryption(b"passphrase"),
     ))
+
+# Various details about who we are. For a self-signed certificate the
+
+# subject and issuer are always the same.
+
+subject = issuer = x509.Name([
+    x509.NameAttribute(NameOID.COUNTRY_NAME, u"US"),
+    x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, u"California"),
+    x509.NameAttribute(NameOID.LOCALITY_NAME, u"San Francisco"),
+    x509.NameAttribute(NameOID.ORGANIZATION_NAME, u"My Company"),
+    x509.NameAttribute(NameOID.COMMON_NAME, u"mysite.com"),
+])
+
+cert = x509.CertificateBuilder().subject_name(
+    subject
+).issuer_name(
+    issuer
+).public_key(
+    key.public_key()
+).serial_number(
+    x509.random_serial_number()
+).not_valid_before(
+    datetime.datetime.utcnow()
+).not_valid_after(
+
+    # Our certificate will be valid for 10 days
+
+    datetime.datetime.utcnow() + datetime.timedelta(days=10)
+).add_extension(
+    x509.SubjectAlternativeName([x509.DNSName(u"localhost")]),
+    critical=False,
+# Sign our certificate with our private key
+).sign(key, hashes.SHA256())
+# Write our certificate out to disk.
+
+with open("/home/toto/crypto/certificate.pem", "wb") as f:
+    f.write(cert.public_bytes(serialization.Encoding.PEM))
