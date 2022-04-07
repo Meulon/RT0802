23460cc0fe4b3b0c15556aedd83342c1e45253c0
meulon <marlon@ik.me>
2022-04-07 20:19:44 +0200

    fix

--- signMsg.py
+++ signMsg.py
@@ -1,12 +1,23 @@
 #!/usr/local/bin/python3
 
-from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
 
-def signMsg(message, privKey):
-    fernet = Fernet(privKey)
-    encMessage = fernet.encrypt(message.encode())
-    return encMessage
+message = b"A message I want to sign"
+private_key = load_privateKey("/home/toto/crypto/key2.pem")
 
-aze = signMsg("azeaze", "/home/toto/crypto/key2.pem")
+def load_privateKey(path):
+    with open(path, 'rb') as f:
+        pem_data = f.read()
+    return serialization.load_pem_private_key(pem_data, password=b"passphrase")
 
-print(aze)
+signature = private_key.sign(
+    message,
+    padding.PSS(
+        mgf=padding.MGF1(hashes.SHA256()),
+        salt_length=padding.PSS.MAX_LENGTH
+    ),
+    hashes.SHA256()
+)
+
+print(base64.b64encode(signature))
