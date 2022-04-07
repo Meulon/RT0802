60739b149c89ed46951404b04d95e11b64ba454f
meulon <marlon@ik.me>
2022-04-07 20:11:32 +0200

    fix

--- signMsg.py
+++ signMsg.py
@@ -2,33 +2,11 @@
 
 from cryptography.fernet import Fernet
 
-# we will be encryting the below string.
-message = "hello geeks"
+def signMsg(message, privKey):
+    fernet = Fernet(privKey)
+    encMessage = fernet.encrypt(message.encode())
+    return encMessage
 
-# generate a key for encryptio and decryption
-# You can use fernet to generate
-# the key or use random key generator
-# here I'm using fernet to generate key
+aze = signMsg("azeaze", "/home/toto/crypto/key2.pem")
 
-key = Fernet.generate_key()
-
-# Instance the Fernet class with the key
-
-fernet = Fernet(key)
-
-# then use the Fernet class instance
-# to encrypt the string string must must
-# be encoded to byte string before encryption
-encMessage = fernet.encrypt(message.encode())
-
-print("original string: ", message)
-print("encrypted string: ", encMessage)
-
-# decrypt the encrypted string with the
-# Fernet instance of the key,
-# that was used for encrypting the string
-# encoded byte string is returned by decrypt method,
-# so decode it to string with decode methods
-decMessage = fernet.decrypt(encMessage).decode()
-
-print("decrypted string: ", decMessage)
+print(aze)
