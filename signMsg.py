3457fa07d7df67232b2de5c37c1993b8c61d7da7
meulon <marlon@ik.me>
2022-04-08 06:22:05 +0200

    git

--- signMsg.py
+++ signMsg.py
@@ -3,7 +3,6 @@
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import serialization
 from cryptography import x509
-from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import hashes
 
