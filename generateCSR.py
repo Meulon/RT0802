fd6bdfd8fac66a06612dfe670493d5ef21c3ac3e
meulon <marlon@ik.me>
2022-04-08 04:56:50 +0200

    fix

--- generateCSR.py
+++ generateCSR.py
@@ -12,6 +12,11 @@ def saveCSR(csr, filename):
         f.write(csr.public_bytes(serialization.Encoding.PEM))
     return "CSR file: " + filename
 
+def loadPrivateKey(path):
+    with open(path, 'rb') as f:
+        pem_data = f.read()
+    return serialization.load_pem_private_key(pem_data, password=b"passphrase")
+
 subject = issuer = x509.Name([
     x509.NameAttribute(NameOID.COUNTRY_NAME, u"FR"),
     x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, u"France"),
@@ -20,8 +25,10 @@ subject = issuer = x509.Name([
     x509.NameAttribute(NameOID.COMMON_NAME, u"strass.fr"),
 ])
 
+privateKey = loadPrivateKey('RSAClient.pem')
+
 # Generate a CSR
-csr = x509.CertificateSigningRequestBuilder().subject_name(subject).sign(RSAkey, hashes.SHA256())
+csr = x509.CertificateSigningRequestBuilder().subject_name(subject).sign(privateKey, hashes.SHA256())
 
 # Write our CSR out to disk.
 
