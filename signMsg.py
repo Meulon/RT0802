9079d5d624e5d71c27f47299f96e70355d465ab6
meulon <marlon@ik.me>
2022-04-08 06:21:36 +0200

    fix

--- signMsg.py
+++ signMsg.py
@@ -1,12 +1,11 @@
 #!/usr/local/bin/python3
 
-from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import serialization
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.serialization import load_pem_public_key
 from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
 
 def signMsg(message, key):
     return key.sign(
